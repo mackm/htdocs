3db9831209572e2ca8dbf5cd29a904adc7ec9e85
Mack McCauley <mack@swiftsuremarketing.com>
2012-03-07 17:54:47 -0800

    page-title in all

--- sites/sites.php
+++ sites/sites.php
@@ -48,5 +48,4 @@ $sites['swiftsurepreview2.com'] = 'www.earthboxinn.com';
 #$sites['swiftsurepreview3.com'] = 'www.birdrockhotel.com';
 $sites['www.swiftsurepreview3.com'] = 'www.swiftsuremarketing.com';
 $sites['swiftsurepreview3.com'] = 'www.swiftsuremarketing.com';
-
-$sites['localhost'] = 'www.visitsanjuans.com';
+$sites['localhost'] = 'www.earthboxinn.com';
