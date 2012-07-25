08bec61bfe0ebe148b16c3d9e15367018e79a291
mackm <mackm@rockisland.com>
2012-07-25 10:30:16 -0700

    add wysiwyg to all/modules

--- sites/sites.php
+++ sites/sites.php
@@ -47,4 +47,4 @@ $sites['swiftsurepreview1.com'] = 'www.visitsanjuans.com';
 $sites['earthboxinn.com'] = 'www.earthboxinn.com';
 $sites['www.swiftsurepreview3.com'] = 'www.birdrockhotel.com';
 $sites['swiftsurepreview3.com'] = 'www.birdrockhotel.com';
-$sites['localhost'] = 'www.swiftsuremarketing.com';
+$sites['localhost'] = 'www.mpt.com';
