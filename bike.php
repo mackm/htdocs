cefab63e7685d8893f385894a50fa7baec908342
mackm <mackm@rockisland.com>
2014-05-20 08:42:58 -0700

    tweaks to remove error messages

---
+++ bike.php
@@ -0,0 +1,107 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta name="verify-v1" content="GNZAKbYHXz+5qUr/K1IfUL5ik+zxckGoepxf+nXizTI=" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Puget Sound Biking: Biking the Puget Sound, San Juan Islands, WA</title>
+<meta name="description" content="Puget Sound Whale Watching: Take your group whale watching in the Puget Sound. Charter a boat today!" />
+<meta name="keywords" content="San Juan whale, whale watching San Juan, San Juan Islands whale, whale watching San Juan Islands, San Juan island whale, Friday Harbor whale watching, Orcas Island whale watching, San Juan Island whale watching, Puget sound whale watching, San Juan Islands orca, Friday Harbor whale watch, San Juan Island whale watch, wale watching on Orcas Island, Puget sound sea kayaking, Friday Harbor Kayaking, San Juan Island kayaking, Friday Harbor sea kayaking, San Juan Island sea kayaking, San Juan kayaking, San Juan sea kayaking, kayaking in the San Juans, San Juans sea kayaking, sea kayaking San Juan Island, San Juan Island activities, San Juan island kayak tour, San Juans kayaking, San Juan kayak tour, sea kayak San Juans" />
+<script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/jqueryslidemenu.css" rel="stylesheet" type="text/css"/>
+<!--[if lte IE 7]>
+<style type="text/css">
+html .jqueryslidemenu{height: 1%;} /*Holly Hack for IE7 and below*/
+</style>
+<![endif]-->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jqueryslidemenu.js"></script>
+<script type="text/JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+<?php include_once '_mobile_redirect.php'; ?>
+</head>
+<body id="bike" onload="MM_preloadImages('images/layout/button_kayaking_f2.png','images/layout/button_whale_f2.png','images/kayaks/reserve_f2.gif')">
+
+  <div id="main">  <?php include ("includes/header.php"); ?><div id="bigPicBox">
+    <div id="whiteBar"></div>
+    <div id="bigPic">
+<img src="images/big_pics/home-bike.jpg" width="915" height="345" alt="Orca"/></div>
+  </div>
+  <div id="content">
+    <div id="leftInner">
+      <div id="leftNav">
+<!--<?php include("includes/left_kayaking.php"); ?>-->
+      </div>
+      <img src="images/layout/left_reserve_inner.png" width="204" height="54" alt="reserve" /><br />
+      <div style="margin-top:15px;margin-bottom:15px;"><a href="https://securewebsession.sanjuanislandoutfitters.com/reservations.php"><img src="images/layout/button_inner_kayak.png" alt="kayak tours" name="button_k" width="81" height="27" id="button_k" onmouseover="MM_swapImage('button_k','','images/layout/button_inner_kayak_f2.png',1)" onmouseout="MM_swapImgRestore()" /></a> &nbsp; &nbsp;<a href="https://securewebsession.sanjuanislandoutfitters.com/reservations.php"><img src="images/layout/button_inner_whale.png" alt="whale watching tours" name="button_ww" width="116" height="27" id="button_ww" onmouseover="MM_swapImage('button_ww','','images/layout/button_inner_whale_f2.png',1)" onmouseout="MM_swapImgRestore()" /></a></div>
+    </div>
+    <!-- end leftHome-->
+    <div id="innerContent">
+      <h1>Biking the San Juan Islands</h1>
+      <p>San Juan Island is known not only for its marine environment, but also for its beautiful roadways and farmlands. Located in this rustic confluence of land and sea, the islands are known for their gently rolling hills, pastoral settings, and scenic rural roads. There is no better way to experience these pastoral settings &amp; farmlands than by bicycle.</p>
+      <p><strong>Equipment</strong><br/>
+      All new and fully tuned Scott hybrid bicycles<br />
+		Chariot bike trailer (up to 2 small children)<br />
+		Trail-a-bike </p>
+        <p><strong>Rentals</strong><br />
+        $25/Half Day 	9am-1pm or 1pm-5pm<br />
+	 	$40/full day 	9am-5pm<br />
+For 2-Day, 3-Day, and week long-rentals, please call for availability and pricing.</p>
+
+<p><strong>Tours</strong><br />
+
+Fully supported and guided cycle day tour of the San Juan Islands.<br/>
+$150/person minimum 4 guests. Includes professional bike guide, bike rental, helmet, lunch and snacks.  </p>
+	  
+<br/><br/><?php include("includes/social_include.php"); ?>
+
+	</div>
+    <!-- end innerContent-->
+    <div class="clear"></div>
+  </div>
+</div>
+  <!-- end main-->
+
+<?php include ("includes/footer.php"); ?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-6277018-1");
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
